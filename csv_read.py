a8be44fffdf6f723c8d6f4ae3a7a95ec94fd5e31
Vidhi Patel <36979263+vidhinileshpatel@users.noreply.github.com>
2018-10-22 17:29:39 -0700

    Update csv_read.py

--- csv_read.py
+++ csv_read.py
@@ -4,3 +4,6 @@ import pandas as pd
 # df is the name of the data frame that we make using pandas.
 df = pandas.read_csv('Enter your file name along with the entire path.')
 print(df)   # view the data frame, to see the contents.
+
+# info() gives the information about the data, its data types, number of columns, memory usage, number of entries
+df.info()
